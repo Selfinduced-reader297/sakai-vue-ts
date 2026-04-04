9fb76654ba793211cb9833f866ed709750828b2c
DumiFlex <94956983+DumiFlex@users.noreply.github.com>
2026-04-04 17:34:55 +0300

    chore: add @semantic-release/npm to bump package.json version on release

--- release.config.ts
+++ release.config.ts
@@ -5,6 +5,7 @@ export default {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
+    ['@semantic-release/npm', { npmPublish: false }],
     '@semantic-release/github',
   ],
 } satisfies Partial<GlobalConfig>
